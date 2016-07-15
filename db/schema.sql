80c62807576d1156e90fd95b6525e1acc97e1e2f
Bobby King <king.robertjoseph@gmail.com>
2016-07-15 17:24:22 -0400

    Update schema to account for post_likes and comment_likes

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,6 @@
 -- Notice the order that the tables have to be dropped
+DROP TABLE IF EXISTS comment_likes;
+DROP TABLE IF EXISTS post_likes;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS users;
@@ -27,4 +29,16 @@ CREATE TABLE comments (
   user_id INT REFERENCES users (user_id),
   post_id INT REFERENCES posts (post_id),
   created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE post_likes (
+  post_like_id SERIAL PRIMARY KEY NOT NULL,
+  post_id INT REFERENCES posts (post_id),
+  user_id INT REFERENCES users (user_id)
+);
+
+CREATE TABLE comment_likes (
+  comment_like_id SERIAL PRIMARY KEY NOT NULL,
+  comment_id INT REFERENCES comments (comment_id),
+  user_id INT REFERENCES users (user_id)
 );
