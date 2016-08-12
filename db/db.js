75186f98e5c83c9c8428315f784009bf9cd47c20
Bobby King <king.robertjoseph@gmail.com>
2016-08-12 16:35:42 -0400

    Change pgConfig to all env variables

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const pg       = require('pg-promise')({});
 
-const pgConfig = {  host: process.env.PG_HOST,
-                    port: process.env.PG_PORT,
-                    database: 'express_blog',
-                    user:process.env.PG_USER,
-                    password: process.env.PG_PASSWORD };
+const pgConfig = {  host:     process.env.DB_HOST,
+                    port:     process.env.DB_PORT,
+                    database: process.env.DB_DATABASE,
+                    user:     process.env.DB_USER,
+                    password: process.env.DB_PASSWORD };
 
 const db       = pg(pgConfig);
 
